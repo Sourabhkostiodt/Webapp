157c6a65d6ca6c1f26a4ef7c628a9b59406d7207
Ritik Jaiswal <ritikjaiswal8888@gmail.com>
2024-05-15 15:34:44 +0530

    how

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ function App() {
     
       <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<Form />} />
         <Route path="/login" element={<Form />} />
         <Route path="/products" element={<Products />} />
